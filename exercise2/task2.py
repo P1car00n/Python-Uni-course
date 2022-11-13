97d2dc73ce4501037e8b417a3d2e78125a65e706
Arthur Z <80679567+P1car00n@users.noreply.github.com>
2022-11-13 16:34:05 +0100

    Finish e2task2

---
+++ exercise2/task2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def sortList(listOfTuples):
+    return sorted(listOfTuples, key=lambda tup: tup[1])
+
+if __name__ == "__main__":
+    print(sortList([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
