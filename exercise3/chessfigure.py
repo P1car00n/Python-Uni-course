54b1d9f405ba0c17e3580c9c2e6c60550ebb2411
Arthur Z <80679567+P1car00n@users.noreply.github.com>
2022-11-15 21:28:24 +0100

    Implement basic inits

--- exercise3/chessfigure.py
+++ exercise3/chessfigure.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 class ChessFigure:
-    def __init__(self, color, name, position) -> None:
+    def __init__(self, figure_cls, color, position) -> None:
+        self.figure_cls = figure_cls # see if needed as self further down the line
         self.color = color
-        self.name = name
+        self.figure_type = figure_cls.FIGURE_TYPE
+        self.number = figure_cls.number
+        figure_cls.number += 1
+        self.name = self.figure_type + ' ' + str(self.number)
         self.position = position
+        
 
     def move(self, position):
         pass
@@ -12,24 +17,59 @@ class ChessFigure:
     def beat(self, position):
         pass
 
+
 class Rook(ChessFigure):
-    pass
+    FIGURE_TYPE = 'rook'
+
+    number = 1
+    def __init__(self, color, position) -> None:
+        ChessFigure.__init__(self, Rook, color, position)
+
 
 class Knight(ChessFigure):
-    pass
+    FIGURE_TYPE = 'knight'
+
+    number = 1
+    def __init__(self, color, position) -> None:
+        ChessFigure.__init__(self, Knight, color, position)
+
 
 class Bishop(ChessFigure):
-    pass
+    FIGURE_TYPE = 'bishop'
+
+    number = 1
+    def __init__(self, color, position) -> None:
+        ChessFigure.__init__(self, Bishop, color, position)
+
 
 class King(ChessFigure):
-    pass
+    FIGURE_TYPE = 'king'
+
+    number = 1
+    def __init__(self, color, position) -> None:
+        ChessFigure.__init__(self, King, color, position)
+
 
 class Queen(ChessFigure):
-    pass
+    FIGURE_TYPE = 'queen'
+
+    number = 1
+    def __init__(self, color, position) -> None:
+        ChessFigure.__init__(self, Queen, color, position)
+
 
 class Pawn(ChessFigure):
-    pass
+    FIGURE_TYPE = 'pawn'
+
+    number = 1 # make private?
+    def __init__(self, color, position) -> None:
+        ChessFigure.__init__(self, Pawn, color, position)
 
 
 if __name__ == '__main__':
-    pass
+    for i in range(5):
+        p1 = Pawn('black', 1)
+        r1 = Rook('white', 1)
+        print(p1.name)
+        print(r1.name)
+    
