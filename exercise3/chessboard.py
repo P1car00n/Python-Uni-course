6efbef311e2ea3f38f6bccc53eb17a0231b7c37f
Arthur Z <80679567+P1car00n@users.noreply.github.com>
2022-11-16 22:20:15 +0100

    Reorganise code; implement creating figures

--- exercise3/chessboard.py
+++ exercise3/chessboard.py
@@ -1,29 +1,70 @@
 #!/usr/bin/env python3
 
-import string # string.ascii_lowercase; map it later using a dictionary?
+import string  # string.ascii_lowercase; map it later using a dictionary?
+import position
+import chessfigure
+
 
 class Chessboard:
     def __init__(self) -> None:
-        self.board = [[column+str(row) for column in string.ascii_lowercase[:8]] for row in range(1, 9)]
+        self.initialize_board()
+        self.existing_figures = {}
+        self.create_figures()
+        self.killed_figures = {}
+
+    def print_board(self):
+        for i in self.board:
+            print(' | ', end='')
+            for j in i:
+                print(j, end=' | ')
+            print()
+
+        for i in self.board:
+            print(i)
+
+    def initialize_board(self):
+        if not position.Position.taken_pisitions:
+            self.board = [[column + str(row) for column in string.ascii_lowercase[:8]]
+                          for row in range(1, 9)]  # {a=1, b =2} map it this way
+        else:
+            # untested
+            self.board = [[column + str(row) if position.Position.check_free(row, column) else column + str(
+                row) for column in string.ascii_lowercase[:8]] for row in range(1, 9)]  # + name from taken positions
+
+    def create_figures(self):
+        for i in range(1, 9):
+            self.existing_figures[f'({2}, {i})'] = chessfigure.Pawn(
+                'white', position.Position(2, i))
+        for i in range(1, 9):
+            self.existing_figures[f'({7}, {i})'] = chessfigure.Pawn(
+                'black', position.Position(7, i))
+        for i in range(1, 9, 7):
+            self.existing_figures[f'({1}, {i})'] = chessfigure.Rook(
+                'white', position.Position(1, i))
+        for i in range(1, 9, 7):
+            self.existing_figures[f'({8}, {i})'] = chessfigure.Rook(
+                'black', position.Position(8, i))
+        for i in range(2, 8, 6):
+            self.existing_figures[f'({1}, {i})'] = chessfigure.Knight(
+                'white', position.Position(1, i))
+        for i in range(2, 8, 6):
+            self.existing_figures[f'({8}, {i})'] = chessfigure.Knight(
+                'black', position.Position(8, i))
+        for i in range(3, 7, 4):
+            self.existing_figures[f'({1}, {i})'] = chessfigure.Bishop(
+                'white', position.Position(1, i))
+        for i in range(3, 7, 4):
+            self.existing_figures['f({8}, {i})'] = chessfigure.Bishop(
+                'black', position.Position(8, i))
+        self.existing_figures[f'({1}, {4})'] = chessfigure.Queen(
+            'white', position.Position(1, 4))
+        self.existing_figures[f'({8}, {4})'] = chessfigure.Queen(
+            'black', position.Position(8, 4))
+        self.existing_figures[f'({1}, {5})'] = chessfigure.King(
+            'white', position.Position(1, 5))
+        self.existing_figures[f'({8}, {5})'] = chessfigure.King(
+            'black', position.Position(8, 5))
 
 
 if __name__ == '__main__':
-    print('''
-
-  _         _   _             _                                               _                   _ 
- | |    ___| |_( )___   _ __ | | __ _ _   _   ___  ___  _ __ ___   ___    ___| |__   ___  ___ ___| |
- | |   / _ \ __|// __| | '_ \| |/ _` | | | | / __|/ _ \| '_ ` _ \ / _ \  / __| '_ \ / _ \/ __/ __| |
- | |__|  __/ |_  \__ \ | |_) | | (_| | |_| | \__ \ (_) | | | | | |  __/ | (__| | | |  __/\__ \__ \_|
- |_____\___|\__| |___/ | .__/|_|\__,_|\__, | |___/\___/|_| |_| |_|\___|  \___|_| |_|\___||___/___(_)
-                       |_|            |___/                                                         
-
-''')
-    ch1 = Chessboard()
-    for i in ch1.board:
-        print(' | ', end='')
-        for j in i:
-            print(j, end=' | ')
-        print()
-    for i in ch1.board:
-        print(i)
-        
+    pass
