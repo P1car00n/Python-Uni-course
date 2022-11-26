4b4872696977fb39ac344193b863f6578570eb24
Arthur Z <80679567+P1car00n@users.noreply.github.com>
2022-11-26 12:45:59 +0100

    Account for user input in task3

--- exercise1/task3.py
+++ exercise1/task3.py
@@ -1,6 +1,5 @@
 def count_vowels (string):
     vowels = [vowel for vowel in string if vowel.lower() in ('a','o','u','e','i')]
     return ("Number of vowels: " + str(len(vowels)))
-    # print(vowels)
 
-print(count_vowels('Test string askdjajii9d92dj awdj9 oooOOOO'))
+print(count_vowels(input('Vowel counter: ')))
