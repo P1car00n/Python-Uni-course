6b1a77513cdadf68371879bf548f300f87112cee
Arthur Z <80679567+P1car00n@users.noreply.github.com>
2022-11-26 13:15:34 +0100

    Fix task1

--- exercise2/task1.py
+++ exercise2/task1.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 import string
+import re
 
 
 def count_words(sentence):
-    words = sentence.translate(str.maketrans(
-        '', '', string.punctuation)).split()
-    count = {word: words.count(word) for word in words}
+    words = re.findall(r'\w+|[^\w\s]', sentence, re.UNICODE)
+    count = {word: words.count(word) for word in words if word not in string.punctuation}
     output = ''
     for key, value in count.items():
         output = output + '{} = {}, '.format(key, value)
@@ -14,4 +14,4 @@ def count_words(sentence):
 
 
 if __name__ == "__main__":
-    print(count_words('Tom, eats, and eats.'))
+    print(count_words(input('Word counter: ')))
