c8b8ed3adbac9c0f2b117e8f7a99e2970772fb79
Arthur Z <80679567+P1car00n@users.noreply.github.com>
2022-12-20 17:45:30 +0100

    Finish task8

---
+++ exercise4/task8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''Consider a generator function that
+generates 10 integers and use it to build an
+array'''
+
+import numpy as np
+
+def generate():
+    for i in range(10):
+        yield i
+
+arr = np.fromiter(generate(), int)
+
+print(arr)
