572fe7675630f752b06ab70a06141ec418933d53
Arthur Z <80679567+P1car00n@users.noreply.github.com>
2023-02-07 15:08:06 +0100

    Finish the task

--- exercise7/coursepoll/polls/urls.py
+++ exercise7/coursepoll/polls/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
@@ -9,3 +10,5 @@ urlpatterns = [
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
