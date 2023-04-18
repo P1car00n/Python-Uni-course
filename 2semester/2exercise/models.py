287abf29497cc7b42c3aef6915e3e92cf4b41cf6
Arthur Z <80679567+P1car00n@users.noreply.github.com>
2023-04-19 00:01:30 +0200

    Formatting improvements

--- 2semester/2exercise/models.py
+++ 2semester/2exercise/models.py
@@ -4,18 +4,25 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
+# potentially add some oop
 
-X, y = fetch_california_housing(return_X_y=True) # now could use it for the google thing, since it's in csv # for predicting may use my fnd where I split the data into training and predicting
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+# now could use it for the google thing, since it's in csv # for
+# predicting may use my fnd where I split the data into training and
+# predicting
+X, y = fetch_california_housing(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
 lrm_cal_intercept = LinearRegression(fit_intercept=True).fit(X_train, y_train)
-lrm_cal_no_intercept = LinearRegression(fit_intercept=False).fit(X_train, y_train)
+lrm_cal_no_intercept = LinearRegression(
+    fit_intercept=False).fit(
+        X_train, y_train)
 
 y_pred = lrm_cal_intercept.predict(X_test)
 
 print(lrm_cal_intercept.score(X_train, y_train))
 print(lrm_cal_no_intercept.score(X_train, y_train))
 print(r2_score(y_test, y_pred))
-#print(lrm_cal_intercept.score(y_test, lrm_cal_intercept.predict(X_test)))
-#print(lrm_cal_no_intercept.score(y_test, lrm_cal_no_intercept.predict(X_test)))
+# print(lrm_cal_intercept.score(y_test, lrm_cal_intercept.predict(X_test)))
+# print(lrm_cal_no_intercept.score(y_test, lrm_cal_no_intercept.predict(X_test)))
