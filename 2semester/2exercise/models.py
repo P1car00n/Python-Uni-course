1cd9e4c2a5ab3c513472190a2f3fff7aeaa8abb7
Arthur Z <80679567+P1car00n@users.noreply.github.com>
2023-04-20 22:05:03 +0200

    Refactor the flow

--- 2semester/2exercise/models.py
+++ 2semester/2exercise/models.py
@@ -36,6 +36,7 @@ class LRM(Model):
 X, y = fetch_california_housing(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+# should put it in a separate module
 # setting thousands since there are numbers like 1,000,111 in the dataset; likewise for dates
 dataset_train = pd.read_csv(
     '/home/arthur/Uni/lab2/data/Google_Stock_Price_Train.csv', thousands=',', date_parser='Date')  # input()
@@ -55,25 +56,32 @@ print("Prediction accuracy (R^2) with intercept; google y_test: ",
       lrm_cal_intercept_google.score(X_dataset_train, y_dataset_train))
 
 
-lrm_cal_intercept = LinearRegression(fit_intercept=True).fit(X_train, y_train)
-lrm_cal_no_intercept = LinearRegression(
-    fit_intercept=False).fit(
-        X_train, y_train)
-
-y_pred_lrm_cal_intercept = get_prediction(lrm_cal_intercept, X_test)
-# Old: y_pred_lrm_cal_no_intercapt = lrm_cal_no_intercept.predict(X_test)
-y_pred_lrm_cal_no_intercept = get_prediction(lrm_cal_no_intercept, X_test)
-
-print("Prediction accuracy (R^2) with intercept: ",
-      lrm_cal_intercept.score(X_train, y_train))
-print("Prediction accuracy (R^2) with no intercept: ",
-      lrm_cal_no_intercept.score(X_train, y_train))
-print("Prediction accuracy (R^2) with intercept for y_test: ",
-      r2_score(y_test, y_pred_lrm_cal_intercept))
-print("Prediction accuracy (R^2) with no intercept for y_test: ",
-      r2_score(y_test, y_pred_lrm_cal_no_intercept))
-# print(lrm_cal_intercept.score(y_test, lrm_cal_intercept.predict(X_test)))
-# print(lrm_cal_no_intercept.score(y_test, lrm_cal_no_intercept.predict(X_test)))
+#lrm_cal_intercept = LinearRegression(fit_intercept=True).fit(X_train, y_train)
+#lrm_cal_no_intercept = LinearRegression(
+#    fit_intercept=False).fit(
+#        X_train, y_train)
+#
+#y_pred_lrm_cal_intercept = get_prediction(lrm_cal_intercept, X_test)
+## Old: y_pred_lrm_cal_no_intercapt = lrm_cal_no_intercept.predict(X_test)
+#y_pred_lrm_cal_no_intercept = get_prediction(lrm_cal_no_intercept, X_test)
+#
+#print("Prediction accuracy (R^2) with intercept: ",
+#      lrm_cal_intercept.score(X_train, y_train))
+#print("Prediction accuracy (R^2) with no intercept: ",
+#      lrm_cal_no_intercept.score(X_train, y_train))
+#print("Prediction accuracy (R^2) with intercept for y_test: ",
+#      r2_score(y_test, y_pred_lrm_cal_intercept))
+#print("Prediction accuracy (R^2) with no intercept for y_test: ",
+#      r2_score(y_test, y_pred_lrm_cal_no_intercept))
+## print(lrm_cal_intercept.score(y_test, lrm_cal_intercept.predict(X_test)))
+## print(lrm_cal_no_intercept.score(y_test, lrm_cal_no_intercept.predict(X_test)))
 
 if __name__ == '__main__':
-    pass
+    lrm_cal_intercept = LRM(X_train, y_train, description='linear regression model with intercept for California housing prices', fit_intercept=True)
+    lrm_cal_no_intercept = LRM(X_train, y_train, description='linear regression model with no intercept for California housing prices', fit_intercept=False)
+    y_pred_lrm_cal_intercept = lrm_cal_intercept.get_prediction(X_test)
+    y_pred_lrm_cal_no_intercept = lrm_cal_no_intercept.get_prediction(X_test)
+    print("Prediction accuracy (R^2) for", lrm_cal_intercept, 'is', lrm_cal_intercept.get_score(X_train, y_train))
+    print("Prediction accuracy (R^2) for", lrm_cal_no_intercept, 'is', lrm_cal_no_intercept.get_score(X_train, y_train))
+    print("Prediction accuracy (R^2) of y_test for", lrm_cal_intercept, 'is', r2_score(y_test, y_pred_lrm_cal_intercept))
+    print("Prediction accuracy (R^2) of y_test for", lrm_cal_no_intercept, 'is', r2_score(y_test, y_pred_lrm_cal_no_intercept))
