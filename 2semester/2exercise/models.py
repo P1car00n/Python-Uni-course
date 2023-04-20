1f39c615ea83adec4ab28cd50510982eb5e32955
Arthur Z <80679567+P1car00n@users.noreply.github.com>
2023-04-20 20:46:54 +0200

    Roughly implement reading from csv

--- 2semester/2exercise/models.py
+++ 2semester/2exercise/models.py
@@ -1,10 +1,13 @@
 import numpy as np
+import pandas as pd
 from sklearn.metrics import r2_score
 from sklearn.datasets import fetch_california_housing
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 # potentially add some oop
+
+
 def get_prediction(model, samples):
     return model.predict(samples)
 
@@ -15,19 +18,41 @@ def get_prediction(model, samples):
 X, y = fetch_california_housing(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+# setting thousands since there are numbers like 1,000,111 in the dataset; likewise for dates
+dataset_train = pd.read_csv(
+    '/home/arthur/Uni/lab2/data/Google_Stock_Price_Train.csv', thousands=',', date_parser='Date')  # input()
+dataset_test = pd.read_csv(
+    '/home/arthur/Uni/lab2/data/Google_Stock_Price_Test.csv', thousands=',', date_parser='Date')
+# here I'm setting what column I want to have predicted
+X_dataset_train = dataset_train.drop(['Close', 'Date'], axis=1)
+y_dataset_train = dataset_train['Close']
+X_dataset_test = dataset_test.drop(['Close', 'Date'], axis=1)
+y_dataset_train = dataset_train['Close']
+lrm_cal_intercept_google = LinearRegression(
+    fit_intercept=True).fit(X_dataset_train, y_dataset_train)
+y_pred_lrm_cal_intercept_google = get_prediction(lrm_cal_intercept_google, X_dataset_test)
+print("Prediction accuracy (R^2) with intercept; google: ",
+      lrm_cal_intercept_google.score(X_dataset_train, y_dataset_train))
+print("Prediction accuracy (R^2) with intercept; google y_test: ",
+      lrm_cal_intercept_google.score(X_dataset_train, y_dataset_train))
+
 
 lrm_cal_intercept = LinearRegression(fit_intercept=True).fit(X_train, y_train)
 lrm_cal_no_intercept = LinearRegression(
     fit_intercept=False).fit(
         X_train, y_train)
 
-y_pred_lrm_cal_intercapt = get_prediction(lrm_cal_intercept, X_test)
+y_pred_lrm_cal_intercept = get_prediction(lrm_cal_intercept, X_test)
 # Old: y_pred_lrm_cal_no_intercapt = lrm_cal_no_intercept.predict(X_test)
-y_pred_lrm_cal_no_intercapt = get_prediction(lrm_cal_no_intercept, X_test)
+y_pred_lrm_cal_no_intercept = get_prediction(lrm_cal_no_intercept, X_test)
 
-print("Prediction accuracy (R^2) with intercept: ", lrm_cal_intercept.score(X_train, y_train))
-print("Prediction accuracy (R^2) with no intercept: ", lrm_cal_no_intercept.score(X_train, y_train))
-print("Prediction accuracy (R^2) with intercept for y_test: ", r2_score(y_test, y_pred_lrm_cal_intercapt))
-print("Prediction accuracy (R^2) with no intercept for y_test: ", r2_score(y_test, y_pred_lrm_cal_no_intercapt))
+print("Prediction accuracy (R^2) with intercept: ",
+      lrm_cal_intercept.score(X_train, y_train))
+print("Prediction accuracy (R^2) with no intercept: ",
+      lrm_cal_no_intercept.score(X_train, y_train))
+print("Prediction accuracy (R^2) with intercept for y_test: ",
+      r2_score(y_test, y_pred_lrm_cal_intercept))
+print("Prediction accuracy (R^2) with no intercept for y_test: ",
+      r2_score(y_test, y_pred_lrm_cal_no_intercept))
 # print(lrm_cal_intercept.score(y_test, lrm_cal_intercept.predict(X_test)))
 # print(lrm_cal_no_intercept.score(y_test, lrm_cal_no_intercept.predict(X_test)))
