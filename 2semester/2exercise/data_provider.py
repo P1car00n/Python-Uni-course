d5e782f7b791246e966a0e096073a5558e789ca4
Arthur Z <80679567+P1car00n@users.noreply.github.com>
2023-05-05 17:32:36 +0200

    Finish taking out the data provider logic from the main module

--- 2semester/2exercise/data_provider.py
+++ 2semester/2exercise/data_provider.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
 
 def getCaliforniaXy(test_size=0.2):
@@ -7,3 +8,25 @@ def getCaliforniaXy(test_size=0.2):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_size)
     return X_train, X_test, y_train, y_test
+
+
+def getGoogleShareXy():
+    # setting thousands since there are numbers like 1,000,111 in the dataset;
+    # likewise for dates
+    dataset_train = pd.read_csv(
+        '/home/arthur/Uni/lab2/data/Google_Stock_Price_Train.csv',
+        thousands=',',
+        date_format='%m/%d/%Y')  # TODO: input()
+    dataset_test = pd.read_csv(
+        '/home/arthur/Uni/lab2/data/Google_Stock_Price_Test.csv',
+        thousands=',',
+        date_format='%m/%d/%Y')
+
+    # here I'm setting what column I want to have predicted
+    X_train = dataset_train.drop(['Close', 'Date'], axis=1)
+    y_train = dataset_train['Close']
+    X_test = dataset_test.drop(['Close', 'Date'], axis=1)
+    y_test = dataset_test['Close']
+    return X_train, X_test, y_train, y_test
+
+# may be reworked with OOP
--- 2semester/2exercise/models.py
+++ 2semester/2exercise/models.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from sklearn.metrics import r2_score
-from sklearn.datasets import fetch_california_housing
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 
 import data_provider
 
@@ -33,27 +31,6 @@ class LRM(Model):
         # **kwargs is for fit_intercept=True
         Model.__init__(self, description,
                        model=LinearRegression(**kwargs).fit(X, y))
-
-
-# X, y = fetch_california_housing(return_X_y=True)
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# should put it in a separate module
-# setting thousands since there are numbers like 1,000,111 in the dataset;
-# likewise for dates
-dataset_train = pd.read_csv(
-    '/home/arthur/Uni/lab2/data/Google_Stock_Price_Train.csv',
-    thousands=',',
-    date_format='%m/%d/%Y')  # input()
-dataset_test = pd.read_csv(
-    '/home/arthur/Uni/lab2/data/Google_Stock_Price_Test.csv',
-    thousands=',',
-    date_format='%m/%d/%Y')
-# here I'm setting what column I want to have predicted
-X_dataset_train = dataset_train.drop(['Close', 'Date'], axis=1)
-y_dataset_train = dataset_train['Close']
-X_dataset_test = dataset_test.drop(['Close', 'Date'], axis=1)
-y_dataset_test = dataset_test['Close']
 
 
 if __name__ == '__main__':
@@ -93,41 +70,42 @@ if __name__ == '__main__':
             y_pred_lrm_cal_no_intercept))
 
     # reinitialize Xs and ys
+    X_train, X_test, y_train, y_test = data_provider.getGoogleShareXy()
 
     # google shares
     lrm_gog_intercept = LRM(
-        X_dataset_train,
-        y_dataset_train,
+        X_train,
+        y_train,
         description='linear regression model with intercept for Google share prices',
         fit_intercept=True)
     lrm_gog_no_intercept = LRM(
-        X_dataset_train,
-        y_dataset_train,
+        X_train,
+        y_train,
         description='linear regression model with no intercept for Google share prices',
         fit_intercept=False)
-    y_pred_lrm_gog_intercept = lrm_gog_intercept.get_prediction(X_dataset_test)
+    y_pred_lrm_gog_intercept = lrm_gog_intercept.get_prediction(X_test)
     y_pred_lrm_gog_no_intercept = lrm_gog_no_intercept.get_prediction(
-        X_dataset_test)
+        X_test)
     print("Prediction accuracy (R^2) for", lrm_gog_intercept,
-          'is', lrm_gog_intercept.get_score(X_dataset_train, y_dataset_train))
+          'is', lrm_gog_intercept.get_score(X_train, y_train))
     print(
         "Prediction accuracy (R^2) for",
         lrm_gog_no_intercept,
         'is',
         lrm_gog_no_intercept.get_score(
-            X_dataset_train,
-            y_dataset_train))
+            X_train,
+            y_train))
     print(
         "Prediction accuracy (R^2) of y_test for",
         lrm_gog_intercept,
         'is',
         r2_score(
-            y_dataset_test,
+            y_test,
             y_pred_lrm_gog_intercept))
     print(
         "Prediction accuracy (R^2) of y_test for",
         lrm_gog_intercept,
         'is',
         r2_score(
-            y_dataset_test,
+            y_test,
             y_pred_lrm_gog_no_intercept))
