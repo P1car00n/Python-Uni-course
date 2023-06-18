be17c2e3293cfb0d84c083c7dd1ae82185bb5eba
P1car00n <80679567+P1car00n@users.noreply.github.com>
2023-06-18 17:43:52 +0200

    Implement data_provider.py

--- 2semester/7exercise/data_provider.py
+++ 2semester/7exercise/data_provider.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
 
@@ -12,3 +13,15 @@ def getBlobsXy(test_size=0.2):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_size)
     return X_train, X_test, y_train, y_test
+
+
+def getClusterXy(test_zise=0.2):
+    np.random. seed(0)
+    n_points_per_cluster = 30000
+    C1 = [-6, -2] + 0.7 * np.random.randn(n_points_per_cluster, 2)
+    C2 = [-2, 2] + 0.3 * np.random.randn(n_points_per_cluster, 2)
+    C3 = [1, -2] + 0.2 * np.random.randn(n_points_per_cluster, 2)
+    C4 = [4, 4] + 0.1 * np.random.randn(n_points_per_cluster, 2)
+    C5 = [5, 0] + 1.4 * np.random.randn(n_points_per_cluster, 2)
+    C6 = [5, 6] + 2.0 * np.random.randn(n_points_per_cluster, 2)
+    X = np.vstack((C1, C2, C3, C4, C5, C6))
