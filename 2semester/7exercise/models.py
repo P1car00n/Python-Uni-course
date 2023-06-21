fca62050825f9070ddded8e69573f140fd8f64ca
P1car00n <80679567+P1car00n@users.noreply.github.com>
2023-06-21 19:57:11 +0200

    Implement clustering for the clusters input data

--- 2semester/7exercise/models.py
+++ 2semester/7exercise/models.py
@@ -55,19 +55,20 @@ if __name__ == '__main__':
         for (model, prediction) in zip(models, predictions):
 
             # Rand index adjusted for chance
-            #rand = adjusted_rand_score(
+            # rand = adjusted_rand_score(
             #    labels_true=y_test, labels_pred=prediction)
-            #print('Rand index adjusted for chance for',
+            # print('Rand index adjusted for chance for',
             #      model, 'is as follows: \n', rand)
 
             # Calinski and Harabasz score
-            cnh = calinski_harabasz_score(X=X, labels=prediction) # model.get_labels()
+            cnh = calinski_harabasz_score(
+                X=X, labels=prediction)  # model.get_labels()
             print('Calinski and Harabasz score for',
                   model, 'is as follows: \n', cnh)
 
             #  Davies-Bouldin score
-            #db = davies_bouldin_score(X=X_train, labels=model.get_labels())
-            #print('Davies-Bouldin score for', model, 'is as follows: \n', db)
+            db = davies_bouldin_score(X, labels=prediction)
+            print('Davies-Bouldin score for', model, 'is as follows: \n', db)
 
     # set Xs and ys
     X = data_provider.getBlobsX()
@@ -95,4 +96,32 @@ if __name__ == '__main__':
         predictions=(
             pred_km_blobs,
             pred_mbkm_blobs
+        ))
+
+    # reset Xs and ys
+    X = data_provider.getClusterX()
+
+    # Clusters
+    # K-Means clustering
+    km_clusters = KM(
+        X,
+        description='a K-Means clustering for clusters',
+        n_clusters=2)
+    pred_km_clusters = km_clusters.get_prediction(X)
+
+    # Mini-Batch K-Means clustering
+    mbkm_clusters = MBKM(
+        X,
+        description='a Mini-Batch K-Means clustering for clusters',
+        n_clusters=2)
+    pred_mbkm_clusters = mbkm_clusters.get_prediction(X)
+
+    printAccuracy(
+        models=(
+            km_clusters,
+            mbkm_clusters
+        ),
+        predictions=(
+            pred_km_clusters,
+            pred_mbkm_clusters
         ))
